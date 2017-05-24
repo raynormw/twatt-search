00ec4656c95ed901bd16c4da728030e1d462c2c5
raynormw <tirtawiryaputra@yahoo.com>
2017-05-24 11:28:53 +0700

    using env done

--- controllers/twatt_controller.js
+++ controllers/twatt_controller.js
@@ -1,19 +1,20 @@
 const OAuth = require('oauth');
+require('dotenv').config();
 
 function search(req, res) {
   var oauth = new OAuth.OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
-      'SCrH6Hddxi87AbnYJ8iQeDg3d',
-      'aNaFysh8TxcPr6e7jNED8FsRLRbcT2WrhxRfYuM5xjm9aZgU39',
+      process.env.CONSUMER_KEY, //consumer key
+      process.env.APPLICATION_SECRET, //application secret
       '1.0A',
       null,
       'HMAC-SHA1'
     );
     oauth.get(
       'https://api.twitter.com/1.1/search/tweets.json?q=hacktiv8&src=typd',
-      '867212291019636736-xjXFmLpJjHyKxZsNoj1s79ad3V4UeSt', //test user token
-      'cnOa30PpyZbiPMzCboA5v7rXOAmpGc6afsfmACYIO6eBi', //test user secret
+      process.env.USER_TOKEN, //user token
+      process.env.USER_SECRET, //user secret
     function (e, data) {
       if (e) console.error(e);
       res.send(data);
