7f922b96552058145f17c7dac980b73b3749ac5b
raynormw <tirtawiryaputra@yahoo.com>
2017-05-24 11:19:42 +0700

    OAuth search done

--- controllers/twatt_controller.js
+++ controllers/twatt_controller.js
@@ -1,5 +1,23 @@
+const OAuth = require('oauth');
+
 function search(req, res) {
-  res.send('twat alive');
+  var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      'SCrH6Hddxi87AbnYJ8iQeDg3d',
+      'aNaFysh8TxcPr6e7jNED8FsRLRbcT2WrhxRfYuM5xjm9aZgU39',
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+    oauth.get(
+      'https://api.twitter.com/1.1/search/tweets.json?q=hacktiv8&src=typd',
+      '867212291019636736-xjXFmLpJjHyKxZsNoj1s79ad3V4UeSt', //test user token
+      'cnOa30PpyZbiPMzCboA5v7rXOAmpGc6afsfmACYIO6eBi', //test user secret
+    function (e, data) {
+      if (e) console.error(e);
+      res.send(data);
+    });
 }
 
 module.exports = {
